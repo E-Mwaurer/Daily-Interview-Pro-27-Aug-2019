76a8d4354a97e9030463f6767f40deb983903f69
EdwardMwaura <edduemwaurer@outlook.com>
2019-08-27 17:56:29 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-1ts2k1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-1ts2k1)
---
+++ index.ts
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>Daily Interview Pro 27-Aug-2019</h1>`;
